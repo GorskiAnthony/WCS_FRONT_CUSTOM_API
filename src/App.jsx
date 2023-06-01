80df0fceb68638a750a7bacee092879636b1a3c7
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-06-01 16:47:52 +0200

    update 💫

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
 	const API = "http://localhost:5500/api/users";
+  
 	const [trainers, setTrainers] = useState([]);
 	const fetchData = () => {
 		fetch(API)
@@ -9,18 +11,70 @@ const App = () => {
 			.then((data) => setTrainers(data.users));
 	};
 
-	fetchData();
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const name = e.target[0].value;
+		/**
+     * Ok avec axios ✅
+		axios
+			.post(API, { name })
+			.then((res) => alert(res.data.message))
+			.then(() => fetchData());
+      */
+
+		// fetch(API, {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// 	body: JSON.stringify({ name }),
+		// })
+		// 	.then((res) => res.json())
+		// 	.then((data) => alert(data.message))
+		// 	.then(() => fetchData());
+
+		try {
+			const res = await fetch(API, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ name }),
+			});
+			const data = await res.json();
+			alert(data.message);
+			fetchData();
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleDelete = (id) => {
+		console.log(`Attention, l'utilisateur ${id} va être supprimé`);
+	};
 
 	return (
 		<div>
 			<h1>Liste des formateurs</h1>
 			<ul>
 				{trainers.map((trainer) => (
-					<li key={trainer.id}>{trainer.name}</li>
+					<li key={trainer.id}>
+						{trainer.name}
+						<button onClick={() => handleDelete(trainer.id)}>
+							modifier
+						</button>
+						<button onClick={() => handleDelete(trainer.id)}>
+							supprimer
+						</button>
+					</li>
 				))}
 			</ul>
 
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="name">Nom du formateur</label>
 				<input
 					type="text"
